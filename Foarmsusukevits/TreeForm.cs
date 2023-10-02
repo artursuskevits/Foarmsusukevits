92174f4da800ee89b36a8316afa6f52ece2d3a10
opilane <opilane@HP-CZC2349HTN>
2023-10-02 14:28:59 +0300

    Add project files.

---
+++ Foarmsusukevits/TreeForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foarmsusukevits
+{
+    public partial class TreeForm: Form
+    {
+        TreeView tree;
+        Button btn;
+        public TreeForm()
+        {
+            this.Height = 600;
+            this.Width = 800;
+            this.Text = "Vorm põhielementiga";
+            tree = new TreeView();
+            tree: Dock = DockStyle.Left;
+            tree.BorderStyle = BorderStyle.Fixed3D;
+            tree.AfterSelect += Tree_AfterSelect;
+            TreeNode treeNode = new TreeNode("Elemendid");
+            tree.Nodes.Add(new TreeNode("Nupp-Button"));
+            btn = new Button();
+            btn.Height = 40;
+            btn.Width = 100;
+            btn.Text = "Valjutada mind";
+            btn.Location = new Point(50, 50);
+            btn.Click += Btn_Click;
+
+
+            tree.Nodes.Add(treeNode);
+            this.Controls.Add(tree);
+        }
+
+        private void Tree_AfterSelect(object? sender, TreeViewEventArgs e)
+        {
+            if (e.Node)
+        }
+
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Foarmsusukevits/myform.cs
@@ -0,0 +1,39 @@
+namespace Foarmsusukevits
+{
+    public partial class myform : Form
+    {
+        Button btn;
+        public myform()
+        {
+
+            //InitializeComponent();
+            this.Height = 200;
+            this.Width = 200;
+            btn = new Button();
+            btn.Height = 40;
+            btn.Width = 40;
+            btn.Text = "Valjutada mind";
+            btn.Location = new Point(10, 20);
+            this.Controls.Add(btn);
+        }
+        public myform(int x, int y,string nimetus)
+        {
+
+            //InitializeComponent();
+            this.Height = y;
+            this.Width = x;
+            btn = new Button();
+            btn.Text = nimetus;
+            btn.Location = new Point(10, 20);
+            this.Controls.Add(btn);
+            btn.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            myform form = new myform();
+            form.ShowDialog()
+        }
+    }
+    
+}
