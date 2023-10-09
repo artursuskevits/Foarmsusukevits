4634a9c761d436928b846fe57130209b438db49e
opilane <opilane@HP-CZC2349HTN>
2023-10-09 15:42:29 +0300

    kolitoo

--- Foarmsusukevits/TreeForm.cs
+++ Foarmsusukevits/TreeForm.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
@@ -13,15 +15,16 @@ namespace Foarmsusukevits
         TreeView tree;
         Button btn;
         Label lbl;
-        TextBox txt_box;
+        TextBox txt_box,txt_box2,txt_box3;
         RadioButton r1, r2;
         CheckBox c1, c2;
         PictureBox pb,pb2;
+        ListBox lb;
         public TreeForm()
         {
 
-            this.Height = 600;
-            this.Width = 800;
+            this.Height = 1000;
+            this.Width = 1000;
             this.Text = "Vorm põhielementiga";
             tree = new TreeView();
             tree: Dock = DockStyle.Left;
@@ -112,9 +115,56 @@ namespace Foarmsusukevits
             this.Controls.Add(pb2);
             pb.Visible = false;
             pb2.Visible = false;
+            //
+            treeNode.Nodes.Add(new TreeNode("Listbox"));
+            lb = new ListBox();
+            lb.Height = 80;
+            lb.Width = 85;
+            lb.Items.Add("RoheLine");
+            lb.Items.Add("Sinine");
+            lb.Items.Add("Hall");
+            lb.Items.Add("Kollane");
+            lb.Location = new Point(150, pb.Location.Y + pb.Height);
+            this.Controls.Add(lb);
+            lb.Visible = false;
+            txt_box2 = new TextBox();
+            txt_box2.BorderStyle = BorderStyle.Fixed3D;
+            txt_box2.Height = 50;
+            txt_box2.Width = 100;
+            txt_box2.Text = "Lisa sõna";
+            txt_box2.Location = new Point(175+lb.Height, pb.Location.Y + pb.Height);
+            txt_box2.KeyDown += new KeyEventHandler(Txt_box_AcceptsTabChanged2);
+            this.Controls.Add(txt_box2);
+            txt_box2.Visible = false;
+            txt_box3 = new TextBox();
+            txt_box3.BorderStyle = BorderStyle.Fixed3D;
+            txt_box3.Height = 50;
+            txt_box3.Width = 100;
+            txt_box3.Text = "Kustuta sõna";
+            txt_box3.Location = new Point(txt_box2.Location.Y + txt_box2.Height+45  , pb.Location.Y + pb.Height);
+            txt_box3.KeyDown += new KeyEventHandler(Txt_box_AcceptsTabChanged3);
+            this.Controls.Add(txt_box3);
+            txt_box3.Visible = false;
+            DataSet ds = new DataSet("Xml faill Mune");
+            ds.ReadXml(@"..\..\..\sample_CustomersOrders.xml");
+            DataGridView dataGrid = new DataGridView();
+            dataGrid.Location = new Point(tree.Width + pb.Width, pb.Location.Y);
+            dataGrid.Height = 200;
+            dataGrid.Width = 300;
+            dataGrid.DataSource = ds;
+            dataGrid.AutoSize = true;
+            dataGrid.AutoGenerateColumns = true;
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            
+            dataGrid.DataMember = "order";
+            
+            this.Controls.Add(dataGrid);
+
+                
+           
         }
 
-       
+
 
         private void Txt_box_AcceptsTabChanged(object? sender, KeyEventArgs e)
         {
@@ -125,9 +175,55 @@ namespace Foarmsusukevits
                 {
                     lbl.Text = txt_box.Text;
                 }
-                
+                else
+                {
+                    string tekst = Interaction.InputBox("Sisseta peakiri", "Uus peakiri", "Uuspeakiri");
+                    if (tekst.Length>0)
+                    {
+                        this.Text = tekst;
+                    }
+                }
             }
             
+        }
+        private void Txt_box_AcceptsTabChanged2(object? sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DialogResult result = MessageBox.Show("Kas sa oled kindel?", "Küsimus", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    int itemCount = lb.Items.Count;
+                    string tekst = txt_box2.Text;
+                    if (tekst.Length>0 && !lb.Items.Contains(tekst) && itemCount<11 )
+                    {
+                        lb.Items.Add(tekst);
+                        lb.Height += 20;
+                    }
+                    
+                }
+                
+            }
+
+        }
+        private void Txt_box_AcceptsTabChanged3(object? sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DialogResult result = MessageBox.Show("Kas sa oled kindel?", "Küsimus", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    string tekst = txt_box3.Text;
+                    if (lb.Items.Contains(tekst))
+                    {
+                        lb.Items.Remove(tekst);
+                        lb.Height -= 5;
+                    }
+
+                }
+
+            }
+
         }
 
         private void Checkbox_Changed(object? sender, EventArgs e)
@@ -224,6 +320,14 @@ namespace Foarmsusukevits
             {
                 pb.Visible = !pb.Visible;
             }
+            
+            else if (e.Node.Text == "Listbox")
+            { lb.Visible = !lb.Visible;
+                txt_box2.Visible = !txt_box2.Visible;
+                txt_box3.Visible = !txt_box3.Visible;
+            }
+            else if (e.Node.Text == "Xml faill Mune") 
+            { }
             tree.SelectedNode = null;
         }
         
